994f0fabb40e33c448f13921e5fa1c0a5f9ec492
Ben <ben@benwiser.com>
2018-01-27 21:47:03 +0000

    Removed tracking with mobile

--- src/ImageBillboard.ts
+++ src/ImageBillboard.ts
@@ -3,38 +3,29 @@ import Tracker from './Tracker';
 const DISTANCE_FACTOR: number = 1.2;
 const ACCELERATION_FACTOR: number = 8.0;
 
-interface Vec3 {
+interface Vec2 {
     x: number;
     y: number;
-    z: number;
 }
 
 class ImageBillboard {
     private image: HTMLImageElement;
     private loaded: boolean = false;
-    private isMobile: boolean = false;
-    private currentAcceleration?: Vec3;
+    private currentAcceleration?: Vec2;
 
     constructor(imagePath: string, public x: number, public y: number, public z: number, public width: number, public height: number) {
         this.image = new Image();
         this.image.onload = (event) => {
             this.loaded = true;
         };
-        this.image.src = imagePath;
         this.currentAcceleration = {
             x: 0,
             y: 0,
-            z: 0,
         };
-        const mobile = false;
+        this.image.src = imagePath;
 
-        this.updateMovement = this.updateMovement.bind(this);
         this.updateTracking = this.updateTracking.bind(this);
-        if (mobile) {
-            window.addEventListener("devicemotion", this.updateMovement);
-        } else {
-            this.updateTracking();
-        }
+        this.updateTracking();
     }
 
     private updateTracking() {
@@ -45,17 +36,6 @@ class ImageBillboard {
         moveField('x');
         moveField('y');
         requestAnimationFrame(this.updateTracking);
-    }
-
-    private updateMovement(event: DeviceMotionEvent) {
-        const SPEED = 10.0;
-        const acceleration = event.accelerationIncludingGravity;
-        const moveField = (field: string) => {
-            this.currentAcceleration[field] += (acceleration[field] - this.currentAcceleration[field]) / SPEED;
-        };
-        moveField('x');
-        moveField('y');
-        moveField('z');
     }
 
     public render(context: CanvasRenderingContext2D): void {
